22e04b42fc557341c1601ef3b7b1c567f0715ad2
Şilan Ekin <93199878+silanekin@users.noreply.github.com>
2024-10-16 10:36:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'partials/_header.php' ?>
+
+    
+    <main>
+        <h1>En çok satan ürünler</h1>
+        <ul>
+            <li>SAmsung</li>
+            <li>iphone</li>
+        </ul>
+    </main>
+
+    <?php include 'partials/_footer.php' ?>
